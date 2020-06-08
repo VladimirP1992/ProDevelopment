4e3a0e26449ad565735d825bd5ba748c1508e89f
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-06-08 13:24:01 +0300

    Модель данных для retrofit и возвращаемого результата (успех, ошибка)

---
+++ app/src/main/java/ru/geekbrains/prodevelopment/mvp/model/data/Translation.kt
@@ -0,0 +1,5 @@
+package ru.geekbrains.prodevelopment.mvp.model.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Translation(@field:SerializedName("text") val translation: String?)
