187769567c54551bf90834f3f9f5fa462736334e
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-06-13 23:28:25 +0300

    Теперь не JavaRX, а coroutines

--- app/src/main/java/ru/geekbrains/prodevelopment/model/data/api/ApiService.kt
+++ app/src/main/java/ru/geekbrains/prodevelopment/model/data/api/ApiService.kt
@@ -1,12 +1,12 @@
 package ru.geekbrains.prodevelopment.model.data.api
 
-import io.reactivex.Observable
+import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
-import ru.geekbrains.prodevelopment.model.data.DataModel
+import ru.geekbrains.prodevelopment.model.data.SearchResult
 
 interface ApiService {
 
     @GET("words/search")
-    fun search(@Query("search") wordToSearch: String): Observable<List<DataModel>>
+    fun searchAsync(@Query("search") wordToSearch: String): Deferred<List<SearchResult>>
 }
