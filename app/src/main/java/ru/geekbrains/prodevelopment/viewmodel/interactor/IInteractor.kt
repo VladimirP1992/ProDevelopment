3e3cfedac5a86c3f60639f158280b9d8e25e73d3
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-06-13 23:35:29 +0300

    Зависимости поставляются Koin, RxJava заменена на корутины

--- app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/IInteractor.kt
+++ app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/IInteractor.kt
@@ -1,8 +1,7 @@
 package ru.geekbrains.prodevelopment.viewmodel.interactor
 
-import io.reactivex.Observable
 
 interface IInteractor<T> {
 
-    fun getData(word: String, fromRemoteSource: Boolean): Observable<T>
+    suspend fun getData(word: String, fromRemoteSource: Boolean): T
 }
--- app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/MainInteractor.kt
+++ app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/MainInteractor.kt
@@ -1,25 +1,22 @@
 package ru.geekbrains.prodevelopment.viewmodel.interactor
 
-import io.reactivex.Observable
-import ru.geekbrains.prodevelopment.di.NAME_LOCAL
-import ru.geekbrains.prodevelopment.di.NAME_REMOTE
-import ru.geekbrains.prodevelopment.model.data.AppState
 import ru.geekbrains.prodevelopment.model.data.DataModel
+import ru.geekbrains.prodevelopment.model.data.SearchResult
 import ru.geekbrains.prodevelopment.model.repository.IRepository
-import javax.inject.Inject
-import javax.inject.Named
 
 
-class MainInteractor @Inject constructor(
-    @Named(NAME_REMOTE) val repositoryRemote: IRepository<List<DataModel>>,
-    @Named(NAME_LOCAL) val repositoryLocal: IRepository<List<DataModel>>
-) : IInteractor<AppState> {
+class MainInteractor(
+    private val repositoryRemote: IRepository<List<SearchResult>>,
+    private val repositoryLocal: IRepository<List<SearchResult>>
+) : IInteractor<DataModel> {
 
-    override fun getData(word: String, fromRemoteSource: Boolean): Observable<AppState> {
-        return if (fromRemoteSource) {
-            repositoryRemote
-        } else {
-            repositoryLocal
-        }.getData(word).map { AppState.Success(it) }
+    override suspend fun getData(word: String, fromRemoteSource: Boolean): DataModel {
+        return DataModel.Success(
+            if (fromRemoteSource) {
+                repositoryRemote
+            } else {
+                repositoryLocal
+            }.getData(word)
+        )
     }
 }
