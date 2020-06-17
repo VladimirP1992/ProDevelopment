e3c067a33858f5f3d4f52cec622e36e25aed41b3
VladimirP1992 <p.vladimir.m@yandex.ru>
2020-06-17 20:19:27 +0300

    Сохранение поиска в БД и получение из БЗ без интернета

--- app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/MainViewModel.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import ru.geekbrains.prodevelopment.model.data.DataModel
-import ru.geekbrains.prodevelopment.utils.parseSearchResults
+import ru.geekbrains.prodevelopment.utils.parseOnlineSearchResults
 import ru.geekbrains.prodevelopment.viewmodel.interactor.MainInteractor
 
 class MainViewModel(private val interactor: MainInteractor) :
@@ -25,7 +25,7 @@ class MainViewModel(private val interactor: MainInteractor) :
 
     //Doesn't have to use withContext for Retrofit call if you use .addCallAdapterFactory(CoroutineCallAdapterFactory()). The same goes for Room
     private suspend fun startInteractor(word: String, isOnline: Boolean) = withContext(Dispatchers.IO) {
-        _mutableLiveData.postValue(parseSearchResults(interactor.getData(word, isOnline)))
+        _mutableLiveData.postValue(parseOnlineSearchResults(interactor.getData(word, isOnline)))
     }
 
     override fun handleError(error: Throwable) {
@@ -33,7 +33,7 @@ class MainViewModel(private val interactor: MainInteractor) :
     }
 
     override fun onCleared() {
-        _mutableLiveData.value = DataModel.Success(null)
+        _mutableLiveData.value = DataModel.Success(null)//TODO Workaround. Set View to original state
         super.onCleared()
     }
 }
--- app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/MainInteractor.kt
+++ app/src/main/java/ru/geekbrains/prodevelopment/viewmodel/interactor/MainInteractor.kt
@@ -3,20 +3,22 @@ package ru.geekbrains.prodevelopment.viewmodel.interactor
 import ru.geekbrains.prodevelopment.model.data.DataModel
 import ru.geekbrains.prodevelopment.model.data.SearchResult
 import ru.geekbrains.prodevelopment.model.repository.IRepository
+import ru.geekbrains.prodevelopment.model.repository.IRepositoryLocal
 
 
 class MainInteractor(
     private val repositoryRemote: IRepository<List<SearchResult>>,
-    private val repositoryLocal: IRepository<List<SearchResult>>
+    private val repositoryLocal: IRepositoryLocal<List<SearchResult>>
 ) : IInteractor<DataModel> {
 
     override suspend fun getData(word: String, fromRemoteSource: Boolean): DataModel {
-        return DataModel.Success(
-            if (fromRemoteSource) {
-                repositoryRemote
-            } else {
-                repositoryLocal
-            }.getData(word)
-        )
+        val dataModel: DataModel
+        if (fromRemoteSource) {
+            dataModel = DataModel.Success(repositoryRemote.getData(word))
+            repositoryLocal.saveToDB(dataModel)
+        } else {
+            dataModel = DataModel.Success(repositoryLocal.getData(word))
+        }
+        return dataModel
     }
 }
